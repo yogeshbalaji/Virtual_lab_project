413d9d08dfbc143d5ca1d256a053a3dfa7c87696
Sabyasachi Shivkumar <sabyashiv@gmail.com>
2014-06-08 19:21:50 +0530

    Creating dropdown menu

---
+++ HTML5Application/public_html/ic_generate.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function select_ic(id)
+{
+var canvas=document.getElementById('myCanvas');
+var ctx=canvas.getContext('2d');
+ctx.fillStyle='#000000';
+ctx.fillRect(0,0,80,100);
+}
